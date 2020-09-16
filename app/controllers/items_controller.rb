2edde75d5fb66305111bba54c3af58f35306efaf
Yoccoi <ba023082@gmail.com>
2020-09-16 14:23:15 +0900

    Userモデル単体テストコード記入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,13 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  
-  def index
-    @items = Item.all.order("created_at DESC")
 
+  def index
+    @items = Item.all.order('created_at DESC')
   end
 
   private
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in?
   end
-
 end
