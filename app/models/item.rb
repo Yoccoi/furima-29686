3e913159015df45375beaec69bdf78cec291c99f
Yoccoi <ba023082@gmail.com>
2020-09-18 09:22:44 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,16 +7,27 @@ class Item < ApplicationRecord
   belongs_to_active_hash :days_for_shipping
   
   belongs_to :user, optional: true
+  has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :content
     validates :image
-    validates :category, numericality: { other_than: 1 }
-    validates :condition, numericality: { other_than: 1 }
+    validates :category
+    validates :condition
     validates :price
-    validates :shipping_fee, numericality: { other_than: 1 }
-    validates :prefecture, numericality: { other_than: 1 }
-    validates :days_for_shipping, numericality: { other_than: 1 }
+    validates :shipping_fee
+    validates :prefecture
+    validates :days_for_shipping
   end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :days_for_shipping_id
+  end
+
+
 end
