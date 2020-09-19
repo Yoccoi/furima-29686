811d7e999bd8c5bc4187aa05daf6a41c5e3b61d8
Yoccoi <ba023082@gmail.com>
2020-09-19 23:02:16 +0900

    ビューファイル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @user = User.find_by(id: @item.user_id)
+    @category = Category.find_by(id: @item.category_id)
+    @condition = Condition.find_by(id: @item.condition_id)
+    @shipping_fee = ShippingFee.find_by(id: @item.shipping_fee_id)
+    @prefecture = Prefecture.find_by(id: @item.prefecture_id)
+    @days = DaysForShipping.find_by(id: @item.days_for_shipping_id)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def move_to_index
