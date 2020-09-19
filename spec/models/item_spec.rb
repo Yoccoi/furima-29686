f7d8754c951f5816c306c7f00288987389a5b807
Yoccoi <ba023082@gmail.com>
2020-09-19 15:05:56 +0900

    [テストコード] プルダウン項目をnilから1に変更

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -25,29 +25,29 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Content can't be blank")
     end
     it 'カテゴリー情報が無いと保存できないこと' do
-      @item.category_id = nil
+      @item.category_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
     end
     it '商品の状態について情報がないと保存できないこと' do
-      @item.condition_id = nil
+      @item.condition_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Condition can't be blank", "Condition is not a number")
+      expect(@item.errors.full_messages).to include("Condition must be other than 1")
     end
     it '配送料の負担について情報がないと保存できないこと' do
-      @item.shipping_fee_id = nil
+      @item.shipping_fee_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Shipping fee can't be blank", "Shipping fee is not a number")
+      expect(@item.errors.full_messages).to include("Shipping fee must be other than 1")
     end
     it '発送元の地域について情報がないと保存できないこと' do
-      @item.prefecture_id = nil
+      @item.prefecture_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
     end
     it '発送までの日数について情報がないと保存できないこと' do
-      @item.days_for_shipping_id = nil
+      @item.days_for_shipping_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Days for shipping can't be blank", "Days for shipping is not a number")
+      expect(@item.errors.full_messages).to include("Days for shipping must be other than 1")
     end
     it '価格がないと保存できないこと' do
       @item.price = nil
