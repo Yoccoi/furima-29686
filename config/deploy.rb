37a2d0693483144c06a38e713011e947e66c9636
Yoccoi <ba023082@gmail.com>
2020-09-28 12:16:11 +0900

    githubリポジトリ名修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima-29686'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Yocooi/furima-29686.git'
+set :repo_url,  'git@github.com:Yoccoi/furima-29686.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
