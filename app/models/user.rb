70ad60c0c35b3bc98c9e51b8c267015fe16e3a96
Yoccoi <ba023082@gmail.com>
2020-09-30 23:46:51 +0900

    SNSログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,26 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   has_many :items
   has_many :orders
+  has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みであるか判断
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
 
   # def Zenkaku
   #   @zenkaku = { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください' }
@@ -18,8 +35,8 @@ class User < ApplicationRecord
     validates :password_confirmation
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' }
-    validates :first_name_kana, format: { with: @zenkaku_kana, message: 'は全角で入力してください' }
-    validates :last_name_kana, format: { with: @zenkaku_kana, message: 'は全角で入力してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角で入力してください' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角で入力してください' }
     validates :birth_date
   end
 end
