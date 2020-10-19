a9c02df1df5208edb0f12c908c4e2d1d280cc94c
Yoccoi <ba023082@gmail.com>
2020-10-19 18:36:17 +0900

    dependent: :destroy追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
 
   belongs_to :user, optional: true
   has_one_attached :image
-  has_one :order
+  has_one :order, dependent: :destroy
 
   with_options presence: true do
     validates :name
